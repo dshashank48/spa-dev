18a31aa5ff253fb6ead77e3612b7bd72ae3764d3
Shashank Dubey <dev20@bytescrum.com>
2022-06-14 12:05:51 +0530

    Add to pdf and download working

--- src/components/ScreenShot.js
+++ src/components/ScreenShot.js
@@ -1,32 +1,38 @@
 import React from "react";
 import html2canvas from "html2canvas";
+import { generatePdfFromImages } from "../functions";
+import jsPDF from "jspdf";
 
 const images = [];
 
 const ScreenShot = ({ playlist, currentSlideIndex, isEnd }) => {
    const capture = () => {
       console.log("log:", playlist[currentSlideIndex]?.type);
-      if (isEnd) {
-         //TODO: Perform pdf download process here and return
-      }
+
       html2canvas(
          document.querySelector(`#${playlist[currentSlideIndex]?.type}`)
       ).then((canvas) => {
-         const image = canvas
-            .toDataURL("image/png", 1.0)
-            .replace("image/png", "image/octet-stream");
-         images.push(image);
-         var link = document.createElement("a");
-         link.download = playlist[currentSlideIndex]?.type + ".png";
-         link.href = image;
-         link.click();
+         var imgData = canvas.toDataURL("image/jpeg", 1.0);
+
+         images.push(imgData);
+
+         console.log("log: iamges", images);
+         if (isEnd) {
+            let pdf = new jsPDF();
+
+            pdf.deletePage(1);
+            images.forEach((image) => {
+               pdf.addPage();
+               pdf.addImage(image, "JPEG", 0, 0);
+            });
+            pdf.save("spa-download.pdf");
+            return;
+         }
       });
    };
 
    return (
-      <button onClick={capture}>
-         {isEnd ? "Download PDF" : "Add to pdf"}{" "}
-      </button>
+      <button onClick={capture}>{isEnd ? "Download PDF" : "Add to pdf"}</button>
    );
 };
 
